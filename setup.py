128a5243e55c3c5e792f224b7fdc2cc21e175774
aman <aman.vishwakarma@petpooja.com>
2025-03-05 18:33:08 +0530

    initial one

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aman Vishwakarma",
+    author_email="aaman2004310100015@gmail.com",
+    packages=find_packages()
+)
